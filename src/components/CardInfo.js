44ee8bb6709d17fffd452e40769278ee49a6b32b
Kriss <krissfrandsen@gmail.com>
2021-05-16 14:50:54 +0200

    make shopping card backbone

---
+++ src/components/CardInfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "./reusableComponents/Button";
+
+const CardInfo = ({ shoePrice, updateCart }) => {
+  return (
+    <div className="cardInfo">
+      <p>{shoePrice} kr</p>
+      <Button addItem={updateCart} />
+    </div>
+  );
+};
+export default CardInfo;
